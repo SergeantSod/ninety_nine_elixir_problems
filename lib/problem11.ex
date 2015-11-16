1f5194a2f93d64df937c4b1a0785db9ad471721b
Johannes Pirkl <johannespirkl@gmx.de>
2015-11-16 20:06:49 +0100

    Phoning in Problem11.

--- lib/problem11.ex
+++ lib/problem11.ex
@@ -1,5 +1,10 @@
 defmodule Problem11 do
   def rle(elements) do
-    Problem10.rle elements
+    elements
+      |> Problem10.rle
+      |> Enum.map(&inline_singleton/1)
   end
+
+  defp inline_singleton({1, x}), do: x
+  defp inline_singleton(other), do: other
 end
