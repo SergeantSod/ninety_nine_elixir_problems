be3259fe85d429fd5a905d79a47b97696355f6be
Johannes Pirkl <johannespirkl@gmx.de>
2015-11-16 20:50:03 +0100

    Gonna do these later.

---
+++ test/problem12_test.exs
@@ -0,0 +1,8 @@
+defmodule Problem12.Test do
+  use ExUnit.Case
+
+  test "TODO" do
+    flunk "Implement the exercise, lazy bum."
+  end
+
+end
---
+++ test/problem14_test.exs
@@ -0,0 +1,8 @@
+defmodule Problem14.Test do
+  use ExUnit.Case
+
+  test "TODO" do
+    flunk "Implement the exercise, lazy bum."
+  end
+
+end
